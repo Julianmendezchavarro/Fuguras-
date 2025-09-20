396bf8dc509061c7f6281690490775b475487979
Julian David Mendez chavarro <julian_mendezch@fet.edu.co>
2025-09-20 12:04:04 -0500

    Update Circulo.java

--- src/Circulo.java
+++ src/Circulo.java
@@ -5,12 +5,10 @@ public class Circulo implements Figura {
         this.radio = radio;
     }
 
-    @Override
     public double calcularArea() {
         return Math.PI * radio * radio;
     }
 
-    @Override
     public double calcularPerimetro() {
         return 2 * Math.PI * radio;
     }
